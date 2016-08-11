d8bb9fb633c12df622c06f461df55d52004cc97d
Felix Kohlgrüber <felix.kohlgrueber@gmail.com>
2016-08-11 22:31:07 +0200

    added MockDisplay class to test.py

--- test.py
+++ test.py
@@ -2,12 +2,24 @@
 from Xlib.ext.xinput import *
 
 
+class MockDisplay(object):
+    def __init__(self, major_ext):
+        self.display = self
+        self.major_ext = major_ext
+
+    def get_extension_major(self, extname):
+        return self.major_ext
+
+    def send_request(self, request, onerror):
+        print_bytes(request._binary)
+
+
 def print_bytes(bytes):
     print(", ".join(hex(v) for v in bytes))
 
 
 for l in range(6):
-    print_bytes(AddMasterInfo.to_binary(name="t"*l))
+    print_bytes(AddMasterInfo.to_binary(name="t" * l))
 
 print()
 
@@ -15,12 +27,5 @@ print_bytes(RemoveMasterInfo.to_binary(deviceid=22))
 print_bytes(AttachSlaveInfo.to_binary(deviceid=9, new_master=2))
 print_bytes(DetachSlaveInfo.to_binary(deviceid=9))
 
+change_hierarchy(MockDisplay(15), {'type': RemoveMaster, 'deviceid': 22})
 
-class a(object):
-    def __init__(self):
-        print("init a")
-
-class b(a):
-    pass
-
-obj = b()
